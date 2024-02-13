7730bc66bc1490f5bb85d2cf2652b3b7f091156d
blancojohn <142819007+blancojohn@users.noreply.github.com>
2024-02-13 12:56:07 +0000

    Terminado

--- src/models.py
+++ src/models.py
@@ -1,29 +1,58 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User  (Base):
+    __tablename__ = 'users'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_name = Column(String(250), unique = True, nullable=False)
+    email = Column(String(250), unique = True, nullable = False)
+    password = Column(String(250), nullable = False)
+    active = Column(Boolean, default=True)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Favorit_Planet(Base):
+    __tablename__ = 'favorites planets'
+    id = Column(Integer, primary_key = True)
+    planets_favorites = Column(String(1000))
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship('User', uselist = False, backref = 'favorites_planets')
+    planet_id = Column(Integer, ForeignKey('planets.id'))
+    planet = relationship('Planet', backref = 'favorites_planets')
+
+class Favorit_People(Base):
+    __tablename__ = 'favorites people'
+    id = Column(Integer, primary_key = True)
+    peoples_favorites = Column(String(1000))
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship('User', backref = 'favorites_people')
+    people_id = Column(Integer, ForeignKey('people.id'))
+    people = relationship('People', backref = 'favorites_people')
+
+class Planet(Base):
+    __tablename__ = 'planets'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    planet_name = Column(String(250), unique = True, nullable=False)
+    diameter = Column(String(250))
+    climate = Column(String(250))
+    terrain = Column(String(250))
+
+class People(Base):
+    __tablename__ = 'people'
+    id = Column(Integer, primary_key = True)
+    people_name = Column(String(200), unique = True, nullable = False)
+    birth_year = Column(String(200))
+    gender = Column(String(200))
+    home_world = Column(String(200))
+    user_id = relationship('User', uselist = True, backref = 'People')
 
     def to_dict(self):
         return {}
